7a91601f677d44dc24d1ad3637a7ec9e94d4d5e3
Harsh2.Bansal <harsh2.bansal@ril.com>
2020-03-12 15:36:21 +0530

    Add lecture 2

---
+++ Lecture2/Code/data_types/numeric/type_conversion.py
@@ -0,0 +1,24 @@
+sample_string = '10010'
+
+str_to_int = int(sample_string)
+print('str_to_int: ', str_to_int)
+
+str_to_int_base2 = int(sample_string, 2)
+print('str_to_int_base2: ', str_to_int_base2)
+
+'''
+float_num = float(-6)
+print('float_num: ',float_num)
+'''
+
+'''
+hexadecimal_num = hex(56)
+print('hexadecimal_num: ', hexadecimal_num)
+'''
+
+'''
+complex_num = complex(1.2, 3)
+print('complex_num: ', complex_num)
+'''
+
+# What is the use for explicit conversions when implicit are already available?
---
+++ Lecture2/Code/operators/identity.py
@@ -0,0 +1,4 @@
+a = 10
+b = 5
+print(a is b) # true if a and b are equal
+print(a is not b) # true if a and b are not equal
