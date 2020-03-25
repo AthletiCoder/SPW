91bd5953a2d609e515d5a0693585ec8e4822d2da
nived <nivedk1998@gmail.com>
2020-03-25 12:27:32 +0530

    resolving Lecture 3 merge conflict

---
+++ Lecture2/Code/operators/assignment.py
@@ -0,0 +1,21 @@
+a = 5
+add, sub, mul, div, rem, expo = 0, 0, 0, 1, 1, 1
+
+add += a # Add value a with add and assign to add
+print(add)
+
+sub -= a # Subtract value a with sub and assign to sub
+print(sub)
+
+mul *= a # Multiply value a with mul and assign to mul
+print(mul)
+
+div /= a # Divide value a with div and assign to div
+print(div)
+
+rem %= a # find remainder of rem with a and assign to rem
+print(rem)
+
+expo **= a # Exponent expo with a and assign to expo
+print(expo)
+
---
+++ Lecture2/Code/operators/bitwise.py
@@ -0,0 +1,8 @@
+a = 5
+b = 6
+print(a & b) # bit by bit AND operation
+print(a | b) # bit by bit OR operation
+print(a ^ b) # bit by bit XOR operation
+print(~a) # complement a, machine reads as 2's complement
+print(a << b) # left-shift operation, shifting by 1 is equal to multiply by 2
+print(a >> b) # right-shift operation, shifting by 1 is equal to divide by 2
